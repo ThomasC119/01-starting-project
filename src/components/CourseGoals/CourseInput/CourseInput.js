c003977ab63e962f35179b3c2ca4bd2a0a8507ad
ThomasC119 <90348324+ThomasC119@users.noreply.github.com>
2022-05-23 22:30:06 -0400

    Back at styling with react
    Back from break and jumping back into styling with react

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -7,7 +7,10 @@ const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = (event) => {
+  const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -26,7 +29,7 @@ const CourseInput = (props) => {
         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
           style={{
-            borderColor: !isValid ? "red" : "black",
+            borderColor: !isValid ? "red" : '#ccc',
             background: !isValid ? "salmon" : "transparent"
           }}
           type="text"
